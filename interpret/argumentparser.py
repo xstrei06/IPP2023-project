d896b9a7bd4e69f2ddb5d3c39732276b42c9a126
Jaroslav Streit <Jarek.Streit@seznam.cz>
2023-04-13 00:56:07 +0200

    Reworked argparser to inherit from argparse.ArgumentParser with overriden error method

--- interpret/argumentparser.py
+++ interpret/argumentparser.py
@@ -3,15 +3,10 @@ import sys
 import os
 
 
-class ArgparseOverride(argparse.ArgumentParser):
-    def error(self, message):
-        self.print_usage(sys.stderr)
-        self.exit(10, '%s: error: %s\n' % (self.prog, message))
-
-
-class ArgumentParser:
+class ArgumentParser(argparse.ArgumentParser):
     def __init__(self):
-        self.parser = None
+        super().__init__(usage='interpret.py [--help] [--source=file] [--input=file]',
+                         add_help=False, formatter_class=argparse.RawTextHelpFormatter)
         self.args = None
         self.source = None
         self.input = None
@@ -21,43 +16,40 @@ class ArgumentParser:
         self.src_opened = False
 
     def set_parser(self):
-        self.parser = ArgparseOverride(usage='interpret.py [--help] [--source=file] [--input=file]',
-                                       add_help=False, formatter_class=argparse.RawTextHelpFormatter)
-        self.parser.add_argument('--help', action="store_true",
-                                 help='Display help message and exit (if no other arguments are specified).')
-        self.parser.add_argument("--source", metavar="file", type=self.__valid_file, required=False,
-                                 help="XML file to be interpreted (if not specified, reads from stdin)")
-        self.parser.add_argument("--input", metavar="file", type=self.__valid_file, required=False,
-                                 help="Program input file (if not specified, reads from stdin)")
-        self.parser.add_argument("--stats", metavar="file", required=False,
-                                 help="File to write statistics to")
-        self.parser.add_argument("--insts", action="store_true", required=False,
-                                 help="Writes number of executed instructions to stats file")
-        self.parser.add_argument("--hot", action="store_true", required=False,
-                                 help="Writes lowest order of most executed instruction to stats file")
-        self.parser.add_argument("--vars", action="store_true", required=False,
-                                 help="Writes number of most accessible variables at one time to stats file")
-        self.parser.add_argument("--frequent", action="store_true", required=False,
-                                 help="Writes most frequent instructions to stats file")
-        self.parser.add_argument("--print", metavar='string', required=False,
-                                 help="Writes passed string to stats file")
-        self.parser.add_argument("--eol", action="store_true", required=False,
-                                 help="Writes end of line to stats file")
-        self.parser.epilog = "Note: At least one of the arguments --source=file or --input=file must be specified."
+        self.add_argument('--help', action="store_true",
+                          help='Display help message and exit (if no other arguments are specified).')
+        self.add_argument("--source", metavar="file", type=self.__valid_file, required=False,
+                          help="XML file to be interpreted (if not specified, reads from stdin)")
+        self.add_argument("--input", metavar="file", type=self.__valid_file, required=False,
+                          help="Program input file (if not specified, reads from stdin)")
+        self.add_argument("--stats", metavar="file", required=False,
+                          help="File to write statistics to")
+        self.add_argument("--insts", action="store_true", required=False,
+                          help="Writes number of executed instructions to stats file")
+        self.add_argument("--hot", action="store_true", required=False,
+                          help="Writes lowest order of most executed instruction to stats file")
+        self.add_argument("--vars", action="store_true", required=False,
+                          help="Writes number of most accessible variables at one time to stats file")
+        self.add_argument("--frequent", action="store_true", required=False,
+                          help="Writes most frequent instructions to stats file")
+        self.add_argument("--print", metavar='string', required=False,
+                          help="Writes passed string to stats file")
+        self.add_argument("--eol", action="store_true", required=False,
+                          help="Writes end of line to stats file")
+        self.epilog = "Note: At least one of the arguments --source=file or --input=file must be specified."
 
     def parse(self):
-        self.args = self.parser.parse_args()
+        self.args = self.parse_args()
         self.source = self.args.source
         self.input = self.args.input
         self.stats_file = self.args.stats
         if self.args.help and len(sys.argv) > 2:
-            self.parser.error("Invalid arguments.")
+            self.error("Invalid arguments.")
         elif self.args.help:
-            self.parser.print_help()
+            self.print_help()
             sys.exit(0)
         if self.input is None and self.source is None:
-            print("Either source or input has to be specified.", file=sys.stderr)
-            sys.exit(10)
+            self.error("Either source or input has to be specified.")
 
     def __valid_file(self, filename):
         if not os.path.isfile(filename):
@@ -86,3 +78,7 @@ class ArgumentParser:
             return inp
         else:
             return None
+
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        self.exit(10, '%s: error: %s\n' % (self.prog, message))
